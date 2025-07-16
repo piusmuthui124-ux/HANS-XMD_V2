defd469924c44e088332f75e402842dde50870cb
HANSTZ <hanstzmd@gmail.com>
2025-07-16 23:35:03 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,27 +4,14 @@
 //GitHub: @Mrhanstz
 //WhatsApp: https://whatsapp.com/channel/0029VasiOoR3bbUw5aV4qB31
 //want more free bot scripts? follow my channel : https://whatsapp.com/channel/0029VasiOoR3bbUw5aV4qB31
-const {
-   spawn
-} = require('child_process')
-const path = require('path')
+
+const { spawn } = require('child_process');
+const path = require('path');
 function start() {
-   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
-   console.log([process.argv[0], ...args].join('\n'))
-   let p = spawn(process.argv[0], args, {
-         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-      })
-      .on('message', data => {
-         if (data == 'reset') {
-            console.log('Restarting Bot...')
-            p.kill()
-            start()
-            delete p
-         }
-      })
-      .on('exit', code => {
-         console.error('Exited with code:', code)
-         if (code == '.' || code == 1 || code == 0) start()
-      })
+   let p = spawn(process.argv[0], [path.join(__dirname, 'main.js'), ...process.argv.slice(2)], {
+      stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+   })
+   .on('message', data => data === 'reset' && (p.kill(), start(), delete p))
+   .on('exit', code => ['.', 1, 0].includes(code) && start());
 }
-start()
+start();
